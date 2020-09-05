305942406d14ff63a332309426326f682326361e
John Hershberg <johnher@gmail.com>
2020-09-05 16:28:22 -0700

    Added video definition

--- src/components/portfolioItem/individualContent/spaceView.component.tsx
+++ src/components/portfolioItem/individualContent/spaceView.component.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import GalleryView from "../galleryView.component";
 import screenshotSpaceView from "../../../Assets/Images_Neu/SpaceView.png";
 import screenshotSpaceView2 from "../../../Assets/Images_Neu/SpaceView_2.png";
+import videoSpaceViewEarth from "/src/Assets/Images_Neu/SpaceView_Earth.mp4";
 
 export default function SpaceView() {
   return (
     <GalleryView>
       <img src={screenshotSpaceView} className="screenshotContentImage" alt="screenshot" />
       <img src={screenshotSpaceView2} className="screenshotContentImage" alt="screenshot" />
+      <video src={videoSpaceViewEarth} />
     </GalleryView>
   );
 }
