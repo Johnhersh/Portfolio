d39c6dd4144dbb9d0df75daa1e0ba8a61ee196ec
John Hershberg <johnher@gmail.com>
2020-09-05 12:29:38 -0700

    Stacking elements

--- src/components/portfolioItem/galleryView.component.tsx
+++ src/components/portfolioItem/galleryView.component.tsx
@@ -1,22 +1,41 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
+import "./galleryView.styles.scss";
 import NextPrevButton from "./next-prevButton.component";
 
 interface Props {}
 
 const GalleryView: FunctionComponent<Props> = ({ children }) => {
+  const [activeItem, setActiveItem] = useState(0);
+
   function handlePrevious() {
-    console.log(`Clicked previous`);
+    if (activeItem > 0) {
+      setActiveItem(activeItem - 1);
+      console.log(`Clicked previous`);
+    }
   }
   function handleNext() {
-    console.log(`Clicked next`);
+    if (activeItem < React.Children.count(children) - 1) {
+      setActiveItem(activeItem + 1);
+      console.log(`Clicked next`);
+    }
   }
 
   return (
-    <div>
-      <NextPrevButton type={"nextButton"} clickFunction={handleNext} />
-      <div className="screenshotImageContainer">{children}</div>
+    <div className={"galleryContainer"}>
       <NextPrevButton type={"previousButton"} clickFunction={handlePrevious} />
+      <div className="galleryImagesContainer">
+        {React.Children.map(children || null, (child, i) => {
+          const id = activeItem === i ? "visible" : "hidden"; // Set appropriate ID to whichever item is active
+
+          return (
+            <div id={id} className={"galleryChild"}>
+              {child}
+            </div>
+          );
+        })}
+      </div>
+      <NextPrevButton type={"nextButton"} clickFunction={handleNext} />
     </div>
   );
 };
