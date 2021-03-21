90300b515379321611d3bf1132e7ea28ae222e33
John Hershberg <johnher@gmail.com>
2021-03-20 21:46:23 -0700

    Changed to using a map. No longer manually placing components.

--- src/pages/homepage.component.tsx
+++ src/pages/homepage.component.tsx
@@ -3,24 +3,8 @@ import PortfolioItem from "../components/portfolioItem/portfolioItem.component";
 import TopBar from "../components/topBar/topBar.component";
 
 import "./homepage.styles.scss";
-import {
-  reactSVG,
-  sassSVG,
-  typescriptSVG,
-  dotnetSVG,
-  reactNativeSVG,
-  postgresSVG,
-  reduxSVG,
-  threejsSVG,
-  GraphQL_SVG,
-} from "../icons";
 
-import AlgoVis from "../components/portfolioItem/individualContent/algoVis.component";
-import PortfolioSite from "../components/portfolioItem/individualContent/portfolioSite.component";
-import FirePointe from "../components/portfolioItem/individualContent/firePointe.component";
-import Chat from "../components/portfolioItem/individualContent/chatSystem.component";
-import CurrencyCalc from "../components/portfolioItem/individualContent/currencyCalc.component";
-import SpaceView from "../components/portfolioItem/individualContent/spaceView.component";
+import { portfolioItems } from "../portfolioItems";
 
 function HomePage(props: any) {
   const topBarSize = "110px";
@@ -30,74 +14,17 @@ function HomePage(props: any) {
       <div className="backgroundGradientContainer" />
       <TopBar size={topBarSize} changeTheme={props.changeTheme} />
       <main id="portfolioItemsContainer" style={{ marginTop: topBarSize }}>
-        <PortfolioItem
-          title={"Chat System"}
-          description={
-            "A chat system done to study Node.js. Backend done with node and express, frontend with React. Using socket.io for the websocket chat, and GraphQL with Postgres for database."
-          }
-          liveLink={{ link: "https://chatapp.johnher.com", title: "Chat App" }}
-          githubLink={{ url: "https://github.com/Johnhersh/chat_server", title: "Github backend" }}
-          githubLinkExtra={{
-            url: "https://github.com/Johnhersh/chat_client",
-            title: "Github frontend",
-          }}
-          icons={[reactSVG, typescriptSVG, postgresSVG, sassSVG, GraphQL_SVG]}>
-          <Chat />
-        </PortfolioItem>
-
-        <PortfolioItem
-          title={"Solar System Visualizer"}
-          description={
-            "A web app for viewing the solar system in 3D. Done with three.js for the purpose of studying web visualization technologies."
-          }
-          githubLink={{ url: "https://github.com/Johnhersh/SpaceView", title: "Github Link" }}
-          liveLink={{ link: "https://space-view.johnher.com", title: "Solar System Visualizer" }}
-          icons={[reactSVG, typescriptSVG, sassSVG, threejsSVG]}>
-          <SpaceView />
-        </PortfolioItem>
-
-        <PortfolioItem
-          title={"Currency Converter"}
-          description={
-            "A currency converter app done in React Native. Purpose of this project was to learn Redux, use multiple APIs, and React-Native animations."
-          }
-          githubLink={{
-            url: "https://github.com/Johnhersh/CurrencyConverter",
-            title: "Github Link",
-          }}
-          liveLink={{ link: "exp://exp.host/@johnhersh/currencyconverter", title: "Expo Link" }}
-          icons={[reactNativeSVG, typescriptSVG, reduxSVG]}>
-          <CurrencyCalc />
-        </PortfolioItem>
-
-        <PortfolioItem
-          title={"Algorithm Visualizer"}
-          description={
-            "Made this project to learn more about algorithms and how they work. By trying to visualize common algorithms, I set out to understand them better."
-          }
-          githubLink={{ url: "https://github.com/Johnhersh/AlgoVis", title: "Github Link" }}
-          liveLink={{ link: "https://algovis.johnher.com", title: "Algorithm Visualizer" }}
-          icons={[reactSVG, sassSVG, typescriptSVG]}>
-          <AlgoVis />
-        </PortfolioItem>
-
-        <PortfolioItem
-          title={"Portfolio Site"}
-          description={"This website. Done as a project to learn React and Sass."}
-          githubLink={{ url: "https://github.com/Johnhersh/Portfolio", title: "Github Link" }}
-          icons={[reactSVG, sassSVG, typescriptSVG]}>
-          <PortfolioSite />
-        </PortfolioItem>
-
-        <PortfolioItem
-          title={"Firepointe"}
-          description={
-            "Firepointe is an app mockup done for a local church. It congregates their posts from various social media outlets like Youtube and Facebook and shows them in a feed. There is also a donation page allowing people to give directly from the app. The app was made in React Native, with the backend done in dotnet for the user management."
-          }
-          githubLink={{ url: "https://github.com/Johnhersh/FirepointE", title: "Github Link" }}
-          icons={[reactNativeSVG, dotnetSVG]}>
-          <FirePointe />
-        </PortfolioItem>
+        {portfolioItems.map((item, i) => (
+          <PortfolioItem
+            title={item.title}
+            description={item.description}
+            liveLink={item.livelink}
+            githubLink={item.githubLink}
+            githubLinkExtra={item.githubLinkExtra}
+            icons={item.icons}>
+            {item.child}
+          </PortfolioItem>
+        ))}
       </main>
     </div>
   );
--- src/portfolioItems.tsx
+++ src/portfolioItems.tsx
@@ -19,7 +19,7 @@ import Chat from "./components/portfolioItem/individualContent/chatSystem.compon
 import CurrencyCalc from "./components/portfolioItem/individualContent/currencyCalc.component";
 import SpaceView from "./components/portfolioItem/individualContent/spaceView.component";
 
-type externalLink = {
+export type externalLink = {
   url: string;
   title: string;
 };
@@ -75,7 +75,7 @@ export const portfolioItems: portfolioItem[] = [
   {
     title: "Portfolio Site",
     description: "This website. Done as a project to learn React and Sass.",
-    githubLink: { url: "https://github.com/Johnhersh/AlgoVis", title: "Github Link" },
+    githubLink: { url: "https://github.com/Johnhersh/Portfolio", title: "Github Link" },
     icons: [reactSVG, sassSVG, typescriptSVG],
     child: <PortfolioSite />,
   },
@@ -83,7 +83,7 @@ export const portfolioItems: portfolioItem[] = [
     title: "Firepointe",
     description:
       "Firepointe is an app mockup done for a local church. It congregates their posts from various social media outlets like Youtube and Facebook and shows them in a feed. There is also a donation page allowing people to give directly from the app. The app was made in React Native, with the backend done in dotnet for the user management.",
-    githubLink: { url: "https://github.com/Johnhersh/FirepointE", title: "Github Link" },
+    githubLink: { url: "", title: "Github Link" },
     icons: [reactNativeSVG, dotnetSVG],
     child: <FirePointe />,
   },
